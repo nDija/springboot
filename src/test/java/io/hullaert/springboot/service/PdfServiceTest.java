8719f924664178b6bf0817e31bd13851d15a538c
vincenthullaert <hullaert.vincent@gmail.com>
2020-11-06 09:00:49 +0100

    feat(app): set travis ci config add tmp dir

--- src/test/java/io/hullaert/springboot/service/PdfServiceTest.java
+++ src/test/java/io/hullaert/springboot/service/PdfServiceTest.java
@@ -19,17 +19,16 @@ public class PdfServiceTest {
     @Test
     public void createPDFTest() throws IOException, DocumentException {
         BitMatrix bitMatrix = barCodeService.generateCode39BarcodeImage(barCode39);
-        String tmpdir = System.getProperty("java.io.tmpdir");
-        System.out.println("Temp file path: " + tmpdir);
-        File file = barCodeService.getAsPNG(tmpdir + barCode39 + ".png", bitMatrix);
+        //String tmpdir = System.getProperty("java.io.tmpdir");
+        //System.out.println("Temp file path: " + tmpdir);
+        File file = barCodeService.getAsPNG("" + barCode39 + ".png", bitMatrix);
         Map<String, String> ticketMap = new HashMap<>();
         ticketMap.put(TicketField.RETIREMENT_DATE, "12/09/2019");
         ticketMap.put(TicketField.CLIENT_NAME, "LISA CHETE");
         ticketMap.put(TicketField.PRODUCT_ID, "1100458");
         ticketMap.put(TicketField.PRODUCT_NAME, "Tablette HUAWEI RE7 Projet BOM M5 10.8'");
-        ticketMap.put(TicketField.CODE_BARE_IMG_SRC, tmpdir + file.getName());
         ticketMap.put(TicketField.CODE39, barCode39);
-        pdfService.createPDF("templateTicket.html", ticketMap, tmpdir);
+        pdfService.createPDF("templateTicket.html", ticketMap, "");
 
     }
 }
